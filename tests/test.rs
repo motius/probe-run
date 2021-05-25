e1b77ff1e91e305d6acb55883339b320f39e71a5
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2021-05-25 11:30:17 +0200

    remove volatile printed time info

--- tests/test.rs
+++ tests/test.rs
@@ -1,20 +1,36 @@
-use insta;
-
 /*
 NOTE: These tests need to be run *manually*, e.g.
 - plug nrf52840 DK in
-- run `cargo test successful_run_has_no_backtrace`
+- run `cargo test -- --ignored`
 */
 
-#[test]
-// this test should not be run by default, as it requires the target hardware to be present
-#[ignore]
-fn successful_run_has_no_backtrace() {
-    let command = std::process::Command::new("cargo")
-        .args(&["run", "--", "--chip", "nRF52840_xxAA", "tests/test_elfs/hello"])
-        .output()
-        .expect("failed to execute process");
+#[cfg(test)]
 
-    let pobe_run_output = std::str::from_utf8(&command.stderr).unwrap();
-    insta::assert_snapshot!(pobe_run_output);
+mod target_tests {
+    use insta;
+
+    #[test]
+    // this test should not be run by default, as it requires the target hardware to be present
+    #[ignore]
+    fn successful_run_has_no_backtrace() {
+        let command = std::process::Command::new("cargo")
+            .args(&["run", "--", "--chip", "nRF52840_xxAA", "tests/test_elfs/hello"])
+            .output()
+            .expect("failed to execute process");
+
+        let probe_run_output = std::str::from_utf8(&command.stderr).unwrap();
+
+        // remove the lines printed during flashing, as they contain timing info that's not always the same
+        let mut truncated_probe_run_output = "".to_string();
+        for line in probe_run_output.lines() {
+            if !line.starts_with("    Finished") &&
+               !line.starts_with("     Running `") {
+                    truncated_probe_run_output.push_str(line);
+                    truncated_probe_run_output.push_str("\n");
+                }
+            }
+
+        insta::assert_snapshot!(truncated_probe_run_output);
+    }
+
 }
