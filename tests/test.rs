0b217ef403d0bfc3b442e6aefe7aec4ac43fe332
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2021-05-25 11:39:30 +0200

    impl run_and_truncate

--- tests/test.rs
+++ tests/test.rs
@@ -5,16 +5,15 @@ NOTE: These tests need to be run *manually*, e.g.
 */
 
 #[cfg(test)]
-
 mod target_tests {
     use insta;
 
-    #[test]
-    // this test should not be run by default, as it requires the target hardware to be present
-    #[ignore]
-    fn successful_run_has_no_backtrace() {
+    /// run probe-run with `args` and truncate the "Finished .. in .." and "Running `...`" flashing output
+    fn run_and_truncate(args: &str) -> String {
+        let args = args.split(" ");
+
         let command = std::process::Command::new("cargo")
-            .args(&["run", "--", "--chip", "nRF52840_xxAA", "tests/test_elfs/hello"])
+            .args(args)
             .output()
             .expect("failed to execute process");
 
@@ -23,14 +22,21 @@ mod target_tests {
         // remove the lines printed during flashing, as they contain timing info that's not always the same
         let mut truncated_probe_run_output = "".to_string();
         for line in probe_run_output.lines() {
-            if !line.starts_with("    Finished") &&
-               !line.starts_with("     Running `") {
-                    truncated_probe_run_output.push_str(line);
-                    truncated_probe_run_output.push_str("\n");
-                }
+            if !line.starts_with("    Finished") && !line.starts_with("     Running `") {
+                truncated_probe_run_output.push_str(line);
+                truncated_probe_run_output.push_str("\n");
             }
+        }
 
-        insta::assert_snapshot!(truncated_probe_run_output);
+        truncated_probe_run_output
     }
 
+    #[test]
+    // this test should not be run by default, as it requires the target hardware to be present
+    #[ignore]
+    fn successful_run_has_no_backtrace() {
+        let run_output = run_and_truncate("run -- --chip nRF52840_xxAA hello");
+
+        insta::assert_snapshot!(run_output);
+    }
 }
