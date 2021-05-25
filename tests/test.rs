9c5123077d3f1fad7ea10b5f90e67e675767215e
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2021-05-25 11:49:02 +0200

    rm tests module so that snap names are the same regardless of run mode

--- tests/test.rs
+++ tests/test.rs
@@ -4,39 +4,46 @@ NOTE: These tests need to be run *manually*, e.g.
 - run `cargo test -- --ignored`
 */
 
-#[cfg(test)]
-mod target_tests {
-    use insta;
+use insta;
 
-    /// run probe-run with `args` and truncate the "Finished .. in .." and "Running `...`" flashing output
-    fn run_and_truncate(args: &str) -> String {
-        let args = args.split(" ");
+/// run probe-run with `args` and truncate the "Finished .. in .." and "Running `...`" flashing output
+fn run_and_truncate(args: &str) -> String {
+    let args = args.split(" ");
 
-        let command = std::process::Command::new("cargo")
-            .args(args)
-            .output()
-            .expect("failed to execute process");
+    let command = std::process::Command::new("cargo")
+        .args(args)
+        .output()
+        .expect("failed to execute process");
 
-        let probe_run_output = std::str::from_utf8(&command.stderr).unwrap();
+    let probe_run_output = std::str::from_utf8(&command.stderr).unwrap();
 
-        // remove the lines printed during flashing, as they contain timing info that's not always the same
-        let mut truncated_probe_run_output = "".to_string();
-        for line in probe_run_output.lines() {
-            if !line.starts_with("    Finished") && !line.starts_with("     Running `") {
-                truncated_probe_run_output.push_str(line);
-                truncated_probe_run_output.push_str("\n");
-            }
+    // remove the lines printed during flashing, as they contain timing info that's not always the same
+    let mut truncated_probe_run_output = "".to_string();
+    for line in probe_run_output.lines() {
+        if !line.starts_with("    Finished") && !line.starts_with("     Running `") &&
+            !line.starts_with("    Blocking waiting for file lock ") {
+            truncated_probe_run_output.push_str(line);
+            truncated_probe_run_output.push_str("\n");
         }
-
-        truncated_probe_run_output
     }
 
-    #[test]
-    // this test should not be run by default, as it requires the target hardware to be present
-    #[ignore]
-    fn successful_run_has_no_backtrace() {
-        let run_output = run_and_truncate("run -- --chip nRF52840_xxAA hello");
+    truncated_probe_run_output
+}
 
-        insta::assert_snapshot!(run_output);
-    }
+#[test]
+// this test should not be run by default, as it requires the target hardware to be present
+#[ignore]
+fn successful_run_has_no_backtrace() {
+    let run_output = run_and_truncate("run -- --chip nRF52840_xxAA hello");
+
+    insta::assert_snapshot!(run_output);
+}
+
+#[test]
+// this test should not be run by default, as it requires the target hardware to be present
+#[ignore]
+fn successful_run_can_enforce_backtrace() {
+    let run_output = run_and_truncate("run -- --chip nRF52840_xxAA hello --force-backtrace");
+
+    insta::assert_snapshot!(run_output);
 }
